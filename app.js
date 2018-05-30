67275ffc749827ac5db524a66fed04a5162a0116
Jeremy Bell <jbell0385@gmail.com>
2018-05-30 09:18:09 -0700

    removed favicon packages

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var favicon = require('express-favicon');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,8 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(favicon(__dirname + '/public/images/favicon.png'))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
